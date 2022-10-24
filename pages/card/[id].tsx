cb24df4112910e15ea4cfae300380d75308413ac
Guilherme Bermêo <gm@bermeo.dev>
2022-10-23 22:55:06 -0300

    🐛 JSON.stringify(printsResponse).trim()
    card/084437ba-26d4-4af6-ab00-dcb145dd2cd0

--- pages/card/[id].tsx
+++ pages/card/[id].tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import type { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import { ParsedUrlQuery } from "querystring";
 import React from "react";
 import CardTags from "../../components/Card/CardTags";
@@ -11,7 +10,7 @@ import { loadCardPrints } from "../../lib/loadCardPrints";
 import { TCard } from "../../types/TCard";
 
 const CardPage: NextPage = ({ card, prints }: any) => {
-  // console.log(prints?.length);
+  // console.log(prints);
 
   return (
     <>
@@ -50,24 +49,20 @@ const CardPage: NextPage = ({ card, prints }: any) => {
               />
             </div>
           </div>
-          <Link href={card.image_uris.art_crop ?? ""}>
+          <a href={card.image_uris.art_crop ?? ""}>
             <img
-              className="my-4 rounded"
-              src={
-                card.image_uris.art_crop
-                  ? card.image_uris.art_crop
-                  : card.image_uris.large
-              }
+              className="my-4 min-w-full rounded"
+              src={card.image_uris.art_crop ?? card.image_uris.large}
               alt={card.name}
             />
-          </Link>
+          </a>
           <div className="flex justify-between font-bold">
             <div className="whitespace-pre-line">{card.type_line}</div>
             <div>{card.set_name}</div>
           </div>
           <div className="p-4 text-lg">
-            <p>{card.oracle_text}</p>
-            <p className="italic">{card.flavor_text}</p>
+            <p className="mb-2 text-sm text-slate-300">{card.oracle_text}</p>
+            <p className="italic">{card.flavor_text ?? ""}</p>
           </div>
 
           <div className="mb-4 flex justify-between">
@@ -81,29 +76,27 @@ const CardPage: NextPage = ({ card, prints }: any) => {
               </div>
             )}
           </div>
-          {prints !== undefined && prints?.length > 0 && (
+          {prints && (
             <>
               <h2 className="mb-2 text-xl font-bold">Other Prints</h2>
               <div className="grid grid-cols-3 gap-4">
-                {prints?.map((print: TCard) => (
+                {prints.map((print: TCard) => (
                   <div
                     className="mb-2 text-center text-xs font-bold"
                     key={print.id}
                   >
                     <div>
-                      <Link href={print.image_uris.large ?? ""}>
+                      <a href={print.image_uris.large ?? ""}>
                         <img
                           className="mb-1 rounded-xl"
                           src={print.image_uris.large}
                           alt={`${print.name} from ${print.set_name} painted by ${print.artist}`}
                         />
-                      </Link>
-                      <Link href={print.related_uris.gatherer ?? ""}>
-                        <>
-                          <p>{print.set_name}</p>
-                          <p>{print.released_at}</p>
-                        </>
-                      </Link>
+                      </a>
+                      <a href={print.related_uris.gatherer ?? ""}>
+                        <p>{print.set_name}</p>
+                        <p>{print.released_at}</p>
+                      </a>
                     </div>
                   </div>
                 ))}
@@ -122,10 +115,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params!;
   const card = await loadCard(id as string);
   const printsResponse: TCard[] = await loadCardPrints(card.prints_search_uri);
-  const prints =
-    printsResponse !== undefined
-      ? JSON.parse(JSON.stringify(printsResponse))
-      : undefined;
+  const stringifiedPrints = JSON.stringify(printsResponse);
+  const trimmedPrints = stringifiedPrints?.trim() || "";
+  const prints: TCard[] = await JSON.parse(trimmedPrints);
+  // printsResponse !== undefined
+  //   ? JSON.parse(JSON.stringify(printsResponse))
+  //   : undefined;
 
   return {
     props: { card, prints },
@@ -141,6 +136,6 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
   // const postList: PostData[] = await GetPosts()
   return {
     paths: paths,
-    fallback: false,
+    fallback: true,
   };
 };
