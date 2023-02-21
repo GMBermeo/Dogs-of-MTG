73785b840958dace5c7fa2b8a10c9dfc1786d1c3
Guilherme Bermêo <gm@bermeo.dev>
2023-02-21 11:43:25 -0300

    ⚗️ Changed Image tag to img (saving on Vercel usage)

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,13 +7,15 @@ export const Footer = () => {
   return (
     <footer className="mb-4 flex w-screen flex-col py-4 px-4 print:hidden md:px-8">
       <div className="flex justify-between">
-        <Link href="/" className="hidden opacity-50 hover:opacity-100 md:block">
-          <Image
+        <Link
+          href="https://dogs-of-mtg.bermeo.dev"
+          className="hidden opacity-50 hover:opacity-100 md:block"
+        >
+          <img
             src="https://dogs-of-mtg.bermeo.dev/Logo.svg"
             height={36}
             width={36}
             alt="The Dogs of Magic of the Gathering Logo"
-            priority
           />
         </Link>
         <div className="my-auto">
@@ -24,21 +27,19 @@ export const Footer = () => {
         </div>
         <div className="flex justify-end gap-x-4 md:gap-x-6">
           <Link href="https://github.com/GMBermeo">
-            <Image
+            <img
               src="https://dogs-of-mtg.bermeo.dev/social/github.svg"
               height={36}
               width={36}
               alt="Github Invertocat Logo"
-              priority
             />
           </Link>
           <Link href="https://www.linkedin.com/in/gmbermeo/">
-            <Image
+            <img
               src="https://dogs-of-mtg.bermeo.dev/social/linkedin.svg"
               height={36}
               width={36}
               alt="Linkedin Logo"
-              priority
             />
           </Link>
         </div>
